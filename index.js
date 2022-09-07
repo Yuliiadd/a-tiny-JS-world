1e5ce864dcb55bb1ea2b1d031d298746f0a13132
Yuliia <yuliiadd@gmail.com>
2022-09-07 12:55:02 +0300

    update pre-oop exercise with classes

--- index.js
+++ index.js
@@ -1,86 +1,74 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-
-
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
-"use strict"
-
-const man = {
-   species: 'Human',
-   name: 'Joe',
-   gender: 'male',
-   legs: 2,
-   hands: 2,
-   saying: 'How you doing?'
-};
-const woman = {
-   species: 'Human',
-   name: 'Monica',
-   gender: 'female',
-   legs: 2,
-   hands: 2,
-   saying: 'I know!'
-};
-const cat = {
-   species: 'cat',
-   name: 'Taras',
-   gender: 'male',
-   legs: 4,
-   hands: 0,
-   saying: 'Meeeeoow!'
-};
-const dog = {
-   species: 'dog',
-   name: 'Chappy',
-   gender: 'male',
-   legs: 4,
-   hands: 0,
-   saying: 'Woooof!'
-};
-
-const catWoman = Object.assign({}, cat);
-catWoman.species = 'Cat-woman';
-catWoman.name = 'Halle Berry';
-catWoman.gender = 'female';
-catWoman.legs = 2;
-catWoman.hands = 2;
-
-const livingBeings = [man, woman, cat, dog, catWoman];
-
-function objToSrting(obj) {
-   let result = "";
-   for (var key in obj) {
-      result += obj[key] + ";"
+class LivingBeing {
+   constructor(species, name, gender, saying) {
+      this.species = species;
+      this.name = name;
+      this.gender = gender;
+      this.saying = saying;
+      this.properties = ['species','name','gender','saying'];
    };
-   print(result.substring(0, result.length - 1));
+
+   showProperties() {
+      return this.properties.map(prop => this[prop]).join('; ');
+   }
 };
+
+class Human extends LivingBeing {
+   constructor(name, gender, saying) {
+      super("Human", name, gender, saying);
+      this.legs = 2;
+      this.hands = 2;
+   };
+
+   showProperties() {
+      return super.showProperties() + `; ${this.legs}; ${this.hands}`;
+   };
+};
+
+class Animal extends LivingBeing {
+   constructor(species, name, gender, saying) {
+      super(species, name, gender, saying);
+      this.legs = 4;
+   };
+
+   showProperties() {
+      return super.showProperties() + `; ${this.legs}`;
+   }
+};
+
+class Cat extends Animal {
+   constructor(name, gender) {
+      super("Kitty", name, gender, "Meooow!");
+
+   };
+};
+
+class Dog extends Animal {
+   constructor(name, gender) {
+      super("Puppy", name, gender, "Woof!");
+   };
+};
+
+class CatWoman extends Cat {
+   constructor(name) {
+      super(name, "Female");
+      this.species = "Cat-woman";
+      this.legs = 2;
+      this.hands = 2;
+   };
+
+   showProperties() {
+      return super.showProperties() + `; ${this.hands} `;
+   }
+};
+
+const 
+man = new Human('Joe', 'male', 'How you doing?'),
+woman = new Human('Monica', 'female', 'I know!'),
+cat = new Cat('Taras', 'male'),
+dog = new Dog('Chappy', 'male'),
+catWoman = new CatWoman('Halle Berry'),
+livingBeings = [man, woman, cat, dog, catWoman];
 
 livingBeings.forEach(item => {
-   objToSrting(item);
+   print(item.showProperties());
 });
